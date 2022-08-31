b2273b492b7af7c73ff1203213b5d8ead5efc7ce
Alexis <exerojas01@gmail.com>
2022-08-30 23:32:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import './App.css' ;
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header>
+      <h1>hola soy el navbar</h1>
+      <nav>
+        <ul className='nav_ul__index' >
+          <li>home</li>
+          <li>categoria</li>
+          <li>categoria</li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
-export default App;
+export default  App;
