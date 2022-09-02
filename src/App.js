9579760dc6de65b7e8e1c4bd85610561f9c03f97
Alexis <exerojas01@gmail.com>
2022-09-01 23:13:17 -0300

    actualizacion

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css' ;
 const App = () => {
   return (
     <header>
-      <h1>hola soy el navbar</h1>
       <nav>
-        <ul className='nav_ul__index' >
+      <h1 className='titulo'>Black-Shop</h1>
+        <ul>
           <li>home</li>
           <li>categoria</li>
           <li>categoria</li>
